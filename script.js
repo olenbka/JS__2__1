20773ae80ca8a2a2c05d52d5f1647d39aee09b09
olga <olenbka@inbox.ru>
2021-06-02 09:07:46 +0200

    new commit

--- script.js
+++ script.js
@@ -1,17 +1,78 @@
-const goods = [
-    { title: 'Shirt', price: 150, img: './images/tshirt.png' },
-    { title: 'Socks', price: 50, img: './images/socks.jpg' },
-    { title: 'Jacket', price: 350, img: './images/jacket.jpg' },
-    { title: 'Shoes', price: 250, img: './images/shoes.jpeg' },
-  ];
-
-  
-  const renderGoodsItem = (title = 'Shirt', price = 200, img) => `<div class="goods-item"><img class="services__icon" width="60" height="60" src="${img}"><h3>${title}</h3><p>${price}</p><button class="button" type="button">Добавить</button></div>`;
-
-  
-  const renderGoodsList = (list) => {
-    let goodsList = list.map(item => renderGoodsItem(item.title, item.price, item.img)).join("");
-    document.querySelector('.goods-list').innerHTML = goodsList;
+class GoodsItem {
+    constructor(img, title, price) {
+      this.img = img;
+      this.title = title;
+      this.price = price;
+      
+    }
+    render() {
+      return `<div class="goods-item"><img class="services__icon" width="60" height="60" src="${this.img}"><h3>${this.title}</h3><p>${this.price}</p></p><button class="button" type="button">Добавить</button></div>`;
+    }
   }
   
-  renderGoodsList(goods);
+  class GoodsList {
+    constructor() {
+      this.goods = [];
+    }
+    fetchGoods() {
+      this.goods = [
+        { title: 'Shirt', price: 150, img: './images/tshirt.png' },
+        { title: 'Socks', price: 50, img: './images/socks.jpg'},
+        { title: 'Jacket', price: 350, img: './images/jacket.jpg' },
+        { title: 'Shoes', price: 250, img: './images/shoes.jpeg' },
+      ];
+    }
+    sum() {
+      let listSum = 0;
+      this.goods.forEach(good => {
+        listSum += good.price;
+      
+      });
+      
+      return listSum;
+    }
+    render() {
+      let listHtml = '';
+      this.goods.forEach(good => {
+        const goodItem = new GoodsItem(good.img, good.title, good.price);
+        listHtml += goodItem.render();
+      });
+      document.querySelector('.goods-list').innerHTML = listHtml;
+    }
+  }
+
+  const list = new GoodsList();
+  list.fetchGoods();
+  list.render();
+  
+  
+  
+  class BasketItem {
+    constructor(title, price, quantity) {
+      this.title = title;
+      this.price = price;
+      this.quantity = quantity;
+    }
+    render() {
+      return `<div class="basket-item"><h3>${this.title}</h3><p>${this.price}</p><p>${this.quantity}</p><button>Delete item</button></div>`;
+    }
+  }
+  
+ 
+  
+  class Basket {
+    constructor() {
+      this.items = [];
+    }
+    
+    addItem(item) {
+      
+    }
+    
+    deleteItem() {
+      
+    }
+    
+    removeAllItems() {
+    }
+}
